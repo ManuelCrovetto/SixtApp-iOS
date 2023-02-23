dcb1008ee13ddb9e1d0482c1fbd90e4f4bf6f4b1
Manuel Crovetto <87724520+ManuelCrovetto@users.noreply.github.com>
2023-02-24 00:04:35 +0100

    Initial Commit

---
+++ Sixt/SixtApp.swift
@@ -0,0 +1,17 @@
+//
+//  SixtApp.swift
+//  Sixt
+//
+//  Created by Manuel Crovetto on 24/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct SixtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
